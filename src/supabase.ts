2a05c579465ce31d725b0e80764e7a8677f79262
jsierrahoopshype <jsierra@hoopshype.com>
2025-11-10 21:45:54 +0100

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL as string
-const anon = import.meta.env.VITE_SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(url, anon, {
-  auth: { persistSession: true, autoRefreshToken: true }
-})
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!,
+  {
+    auth: { persistSession: false },
+  }
+)
